660ebd8e20bb1e2f91159bcf56ade8299ecb6980
tuanatran <tuantran@uw.edu>
2013-07-29 17:29:05 -0700

    continuous tracking

--- ShowMyLocationOnMap/MainPage.xaml.cs
+++ ShowMyLocationOnMap/MainPage.xaml.cs
@@ -21,70 +21,14 @@ namespace ShowMyLocationOnMap
 {
     public partial class MainPage : PhoneApplicationPage
     {
-       Geolocator myGeolocator = null;
-
+        Geolocator geolocator = null;
+        bool tracking = false;
         // Constructor
         public MainPage()
         {
             InitializeComponent();
             ShowMyLocationOnTheMap();
                       
-        }
-
-        private void TrackLocation()
-        {
-            myGeolocator = new Geolocator();
-            myGeolocator.DesiredAccuracy = PositionAccuracy.High;
-            myGeolocator.MovementThreshold = 100; // The units are meters. 
-            //myGeolocator.StatusChanged += geolocator_StatusChanged;
-            //myGeolocator.PositionChanged += geolocator_PositionChanged;
-        }
-
-        void geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
-        {
-            string status = "";
-
-            switch (args.Status)
-            {
-                case PositionStatus.Disabled:
-                    // the application does not have the right capability or the location master switch is off
-                    status = "location is disabled in phone settings";
-                    break;
-                case PositionStatus.Initializing:
-                    // the geolocator started the tracking operation
-                    status = "initializing";
-                    break;
-                case PositionStatus.NoData:
-                    // the location service was not able to acquire the location
-                    status = "no data";
-                    break;
-                case PositionStatus.Ready:
-                    // the location service is generating geopositions as specified by the tracking parameters
-                    status = "ready";
-                    break;
-                case PositionStatus.NotAvailable:
-                    status = "not available";
-                    // not used in WindowsPhone, Windows desktop uses this value to signal that there is no hardware capable to acquire location information
-                    break;
-                case PositionStatus.NotInitialized:
-                    // the initial state of the geolocator, once the tracking operation is stopped by the user the geolocator moves back to this state
-
-                    break;
-            }
-            Dispatcher.BeginInvoke(() =>
-            {
-                //StatusTextBlock.Text = status;
-            });
-
-        }
-
-        void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args) 
-        { 
-            Dispatcher.BeginInvoke(() =>
-            { 
-               // LatitudeTextBlock.Text = args.Position.Coordinate.Latitude.ToString("0.00");
-               // LongitudeTextBlock.Text = args.Position.Coordinate.Longitude.ToString("0.00");
-            });
         }
 
 
@@ -97,10 +41,7 @@ namespace ShowMyLocationOnMap
             }
             else
             {
-                MessageBoxResult result =
-                    MessageBox.Show("This app accesses your phone's location. Is that ok?",
-                    "Location",
-                    MessageBoxButton.OKCancel);
+                MessageBoxResult result = MessageBox.Show("This app accesses your phone's location. Is that ok?","Location", MessageBoxButton.OKCancel);
 
                 if (result == MessageBoxResult.OK)
                 {
@@ -133,7 +74,7 @@ namespace ShowMyLocationOnMap
         private async void ShowMyLocationOnTheMap()
         {
             // Get my current location.
-            myGeolocator = new Geolocator();
+            Geolocator myGeolocator = new Geolocator();
             Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
             Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
             GeoCoordinate myGeoCoordinate = 
@@ -175,5 +116,86 @@ namespace ShowMyLocationOnMap
             mapsDirectionsTask.End = endLocationLML;
             mapsDirectionsTask.Show(); 
         }
+
+        void geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
+        {
+            string status = "";
+
+            switch (args.Status)
+            {
+                case PositionStatus.Disabled:
+                    // the application does not have the right capability or the location master switch is off
+                    status = "location is disabled in phone settings";
+                    break;
+                case PositionStatus.Initializing:
+                    // the geolocator started the tracking operation
+                    status = "initializing";
+                    break;
+                case PositionStatus.NoData:
+                    // the location service was not able to acquire the location
+                    status = "no data";
+                    break;
+                case PositionStatus.Ready:
+                    // the location service is generating geopositions as specified by the tracking parameters
+                    status = "ready";
+                    break;
+                case PositionStatus.NotAvailable:
+                    status = "not available";
+                    // not used in WindowsPhone, Windows desktop uses this value to signal that there is no hardware capable to acquire location information
+                    break;
+                case PositionStatus.NotInitialized:
+                    // the initial state of the geolocator, once the tracking operation is stopped by the user the geolocator moves back to this state
+
+                    break;
+            }
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                StatusTextBlock.Text = status;
+            });
+        }
+
+        void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                LatitudeTextBlock.Text = args.Position.Coordinate.Latitude.ToString("0.00");
+                LongitudeTextBlock.Text = args.Position.Coordinate.Longitude.ToString("0.00");
+            });
+        }
+
+        private void TrackLocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)IsolatedStorageSettings.ApplicationSettings["LocationConsent"] != true)
+            {
+                // The user has opted out of Location.
+                return;
+            }
+
+            if (!tracking)
+            {
+                geolocator = new Geolocator();
+                geolocator.DesiredAccuracy = PositionAccuracy.High;
+                geolocator.MovementThreshold = 100; // The units are meters.
+
+                geolocator.StatusChanged += geolocator_StatusChanged;
+                geolocator.PositionChanged += geolocator_PositionChanged;
+
+                tracking = true;
+                TrackLocationButton.Content = "stop tracking";
+            }
+            else
+            {
+                geolocator.PositionChanged -= geolocator_PositionChanged;
+                geolocator.StatusChanged -= geolocator_StatusChanged;
+                geolocator = null;
+
+                tracking = false;
+                TrackLocationButton.Content = "track location";
+                StatusTextBlock.Text = "stopped";
+            }
+        }
+
+
     }
 }
